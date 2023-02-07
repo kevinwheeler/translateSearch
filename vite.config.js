d73648325db5537606dd48da51d0dbdd30668f12
Kevin Wheeler <spreadlove5683@gmail.com>
2023-02-07 15:55:40 +0000

    updated vite config and composer lock

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/googleStuff.js', 'resources/js/helpers.js', 'resources/js/homepage.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/googleStuff.js', 'resources/js/helpers.js', 'resources/js/homepage.js', 'resources/js/resultsPageRecaptcha.js', 'resources/js/resultsGET.js'],
             refresh: true,
         }),
     ],
